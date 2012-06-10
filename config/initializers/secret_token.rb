111c3502c5ae477e330de0621410cc3a3e2dd330
Pablo Villalba <pablo@teambox.com>
2012-06-10 15:16:27 +0200

    Original rails 3.2 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Diphub::Application.config.secret_token = 'c11f787cfc6061557f7df76de991839ad2470ab5412bb991701ff43aa13c4f761d0c5f6137a86215a947612928267f0433e0e7ac4c08f93ce389a85af98afac5'
